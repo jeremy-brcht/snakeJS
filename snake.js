a868608a2e4834ea0329b2d11f6000c2f424429e
Jbminers <jejefish69@gmail.com>
2022-03-30 20:13:39 +0200

    deplacements + taille + nouriture
    Pas de gestion de collision + bug de génération de nourriture sur le serpent.

--- snake.js
+++ snake.js
@@ -3,11 +3,16 @@ var ctx = [];
 var ctx = canvas.getContext("2d");
 
 ctx.strokeStyle = "black";
-ctx.fillStyle = "#00FF00";
 
 let grid = [];
+let direction = "none";
 
-function displayGrid(grid) {
+let snake = [];
+let food = [];
+
+let score = 0;
+
+function displayGrid() {
   let cellSize = canvas.width / grid.length;
 
   for (i in grid) {
@@ -21,7 +26,6 @@ function displayGrid(grid) {
           break;
         case "snake":
           ctx[k].fillStyle = "#63aa30";
-          console.log(i, j);
           break;
         case "food":
           ctx[k].fillStyle = "#ff3711";
@@ -29,7 +33,7 @@ function displayGrid(grid) {
       }
       ctx[k].rect(i * cellSize, j * cellSize, cellSize, cellSize);
       ctx[k].fill();
-      ctx[k].stroke();
+      ctx.stroke();
     }
   }
 }
@@ -43,7 +47,7 @@ function createEmptyGrid(size) {
   }
 }
 
-function addToGrid(grid) {
+function addToEmptyGrid() {
   xSnake = Math.floor(Math.random() * grid.length);
   ySnake = Math.floor(Math.random() * grid.length);
 
@@ -53,11 +57,89 @@ function addToGrid(grid) {
   } while (xSnake !== xFood && ySnake !== yFood);
 
   grid[xSnake][ySnake] = "snake";
+  snake.push([xSnake, ySnake]);
+
   grid[xFood][yFood] = "food";
+  food[0] = xFood;
+  food[1] = yFood;
 }
 
-createEmptyGrid(20);
-addToGrid(grid);
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 37:
+      direction = "left";
+      break;
+    case 38:
+      direction = "up";
+      break;
+    case 39:
+      direction = "right";
+      break;
+    case 40:
+      direction = "down";
+      break;
+  }
+};
 
-displayGrid(grid);
+function updateGrid() {
+  for (i in grid) {
+    for (j in grid[i]) {
+      if (grid[i][j] === "snake") {
+        grid[i][j] = "vide";
+      }
+      for (k in snake) {
+        if (snake[k][0].toString() === i && snake[k][1].toString() === j) {
+          grid[i][j] = "snake";
+        }
+      }
+    }
+  }
+
+  console.log(grid);
+  displayGrid();
+}
+
+function newFood() {
+  do {
+    xFood = Math.floor(Math.random() * grid.length);
+    yFood = Math.floor(Math.random() * grid.length);
+  } while (snake.includes([xFood, yFood]));
+
+  grid[xFood][yFood] = "food";
+  food[0] = xFood;
+  food[1] = yFood;
+}
+
+function move() {
+  if (direction !== "none") {
+    switch (direction) {
+      case "right":
+        snake.unshift([snake[0][0] + 1, snake[0][1]]);
+        break;
+      case "down":
+        snake.unshift([snake[0][0], snake[0][1] + 1]);
+        break;
+      case "left":
+        snake.unshift([snake[0][0] - 1, snake[0][1]]);
+        break;
+      case "up":
+        snake.unshift([snake[0][0], snake[0][1] - 1]);
+        break;
+    }
+    if (snake[0].toString() !== food.toString()) {
+      snake.pop();
+    } else {
+      newFood();
+      score++;
+    }
+    updateGrid();
+  }
+}
+
+setInterval(move, 500);
+
+createEmptyGrid(20);
+addToEmptyGrid();
+
+displayGrid();
 console.log(grid);
